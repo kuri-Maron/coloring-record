57d2ab83e3260c43a2daac8010499d6d32d5c28c
Shun-Maron <kurisyun4321@gmail.com>
2020-02-17 21:09:13 +0900

    add: 配列のバグがあるが、一旦コミット

---
+++ src/components/divRecord.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="flexBox">
+      <div v-for="(cell,index) in dataLists"
+        class="flexItem"
+        @click="cellSelect(index)"
+        :class="{cellCheck: cell.isactive}" :key="index"
+      >{{`index:${index} val:${cell.isactive}`}}</div>
+    </div>
+    <p>{{dataLists}}</p>
+  </div>
+</template>
+
+<script>
+// const datas = new Array(12);
+// datas.fill(1);
+
+export default {
+  name: "divRecord",
+  data() {
+    return {
+      // dataLists: datas,
+      dataLists: Array(12),
+      hoge: "hoge"
+    };
+  },
+  created() {
+    // this.dataLists = this.dataLists.fill(null).map((_,i) => i+1).map(month => month + '月');
+    // TODO: 配列が参照渡しになっているので個別にオブジェクトを生成
+    this.dataLists = this.dataLists.fill({ isactive: false, task: "none" });
+  },
+  methods: {
+    cellSelect(index) {
+      if (this.dataLists[index].isactive === false) {
+          this.dataLists[index].isactive = true;
+          this.dataLists[index].task = 'work';
+      } else {
+        this.dataLists.splice(index, 1);
+        this.dataLists.push({ isactive: false, task: 'none' });
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+div.flexBox {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100vw;
+}
+
+.flexItem {
+  background-color: whitesmoke;
+  width: 25vw;
+  border: solid 1px;
+}
+
+.cellCheck {
+  background-color: palegreen;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <record/> -->
+    <div-record/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import Record from '@/components/Record.vue'
+import divRecord from '@/components/divRecord.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+    // Record
+    divRecord
   }
 }
 </script>
