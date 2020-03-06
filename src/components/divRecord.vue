400803eece7bab03da26e461723c97272a5302d9
Shun-Maron <kurisyun4321@gmail.com>
2020-03-07 07:30:58 +0900

    firebaseのfirestoreのロジック追加

--- src/components/divRecord.vue
+++ src/components/divRecord.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <div class="flexBox">
-      <div v-for="(cell,index) in dataLists"
-            class="flexItem"
-            @click="cellSelect(index)"
-            :class="{cellCheck: cell.isActive}" :key="index">
-            {{`index:${index} val:${cell.isActive}`}}
-      </div>
+      <div
+        v-for="(cell, index) in dataLists"
+        class="flexItem"
+        @click="cellSelect(index)"
+        :class="{ cellCheck: cell.isActive }"
+        :key="index"
+      >{{ `index:${index} val:${cell.isActive}` }}</div>
     </div>
-    <!-- <p>{{dataLists}}</p> -->
+    <p>{{ dataLists }}</p>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
 // const datas = new Array(12);
 // datas.fill(1);
 
@@ -25,17 +28,45 @@ export default {
       hoge: "hoge"
     };
   },
-  created() {
+  async created() {
     // TODO: for文ダサいので、forerchかもっと良きメソッド使う。
-    for(let i=0; i<this.dataLists.length; i++){
-        this.$set(this.dataLists,i,{isActive: false,task: 'none'})
+    for (let i = 0; i < this.dataLists.length; i++) {
+      this.$set(this.dataLists, i, { isActive: false, task: "none" });
     }
-// TODO: for文を書かない方法を模索中
+
+    const db = firebase.firestore();
+    let querySnapshot = await db
+      .collection("myRecords")
+      .doc("blocksState")
+      .collection("blocks")
+      .get();
+    await querySnapshot.docs.forEach((doc, index) => {
+      // console.log("index: ", index, doc.id, " => ", doc.data());
+      this.$set(this.dataLists, index, doc.data());
+    });
+
+    // TODO: for文を書かない方法を模索中
     // this.dataLists.forEach((item, i, array) => {
     //   // item = {isActive: false,task: 'none'}
     //   item = false;
     //   array[i] = item;
     // });
+  },
+  async beforeDestroy() {
+    const db = firebase.firestore();
+    let collectionReference = await db
+      .collection("myRecords")
+      .doc("blocksState")
+      .collection("blocks");
+    let querySnapshot = await collectionReference.get();
+    for (let i = 0; i < this.dataLists.length; i++) {
+      if (querySnapshot.size > i) {
+        console.log(querySnapshot.docs[i]);
+        collectionReference.doc(querySnapshot.docs[i].id).update(this.dataLists[i]);
+      } else {
+        collectionReference.add(this.dataLists[i]);
+      }
+    }
   },
   methods: {
     cellSelect(index) {
--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+// console.log('エラー検知フラグ');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJv9rgteA7J72JsDnxmJmlg7BDMIPgWIg",
+  authDomain: "color-record-test.firebaseapp.com",
+  databaseURL: "https://color-record-test.firebaseio.com",
+  projectId: "color-record-test",
+  storageBucket: "color-record-test.appspot.com",
+  messagingSenderId: "1047522148283",
+  appId: "1:1047522148283:web:2861562b5a158cec2cce9e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
